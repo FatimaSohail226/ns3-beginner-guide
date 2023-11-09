b83edae3d01ba37ca17eb40043da0e0e706216e8
FatimaSohail226 <93187994+FatimaSohail226@users.noreply.github.com>
2023-11-09 08:55:31 +0500

    Create hello_world.cc
    This program is the starting point of your ns3 journey. Here we print a simple hello world to the console

---
+++ beginner/hello_world.cc
@@ -0,0 +1,15 @@
+#include "ns3/core-module.h"  // Include the core module of ns-3
+
+using namespace ns3;  // Import the ns3 namespace into the current scope
+
+int main()
+{
+    // Initialize the ns-3 system
+
+    // Print "Hello World!" to the console using the ns-3 logging system
+    // NS_LOG_UNCOND is a macro for logging messages
+    NS_LOG_UNCOND("Hello World!");
+
+    // Exit the program with a return value of 0 (indicating success)
+    return 0;
+}
